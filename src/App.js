bd6796ad0ad16b5955dc3059a761b97da6445bd1
yemsrach-sisay <97808898+yemsrach-sisay@users.noreply.github.com>
2024-07-09 14:26:35 -0400

    simple-react-application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+function Header() {
+  return <h1>Simple React Application</h1>;
+}
+
+function Content(props) {
+  return <p style={{ color: props.color }}>{props.text}</p>;
+}
+
+function Footer() {
+  return <h1>Created by Me, of course.</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Content color="blue" text="This is my first React Application!" />
+      <Content color="red" text="Wish me luck..." />
+      <Content color="green" text="I think I've got it!" />
+      <Footer />
+    </>
   );
 }
 
